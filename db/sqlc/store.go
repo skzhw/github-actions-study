51f4c68b007a01a48b2aeab84bcd5477b4b5b707
skzhw <skhhdsrl6@gmail.com>
2021-08-22 20:12:04 +0900

    first commit

---
+++ db/sqlc/store.go
@@ -0,0 +1,148 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store proviedes all functions to execute db queries and transactions
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new Store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// execTx executes a function within a database transaction
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+	}
+
+	return tx.Commit()
+}
+
+// TransferTx performs a money transfer from one account to the other.
+// It creates a transfer record, add account entries, and update account' balance within a single database transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+// TransferTxResult is the result of the transfer transaction
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+var txKey = struct{}{}
+
+// TransferTx performs a money transfer from one account to the other.
+// It creates a transfer record, add account entries, and update accounts' balance within a single database transaction
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	// 空のTransferTxResult
+	var result TransferTxResult
+
+	// contextと Queriesを引数としてコールバック関数の中で
+	// resultのプロパティに情報をつめていく
+	// エラー出た瞬間元に戻す
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		txName := ctx.Value(txKey)
+		fmt.Println(txName, "create transfer")
+		// TODO: ここがなぞ
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID, // 受ける人
+			ToAccountID:   arg.ToAccountID,   // 送る人
+			Amount:        arg.Amount,        // 金額
+		})
+		if err != nil {
+			return err
+		}
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID, //
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// TODO: update account's balance
+		// result.FromAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		// 	ID:     arg.FromAccountID,
+		// 	Amount: -arg.Amount,
+		// })
+		// if err != nil {
+		// 	return err
+		// }
+		// result.ToAccount, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		// 	ID:     arg.ToAccountID,
+		// 	Amount: arg.Amount,
+		// })
+		// if err != nil {
+		// 	return err
+		// }
+
+		// return nil
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
+		return err
+	})
+
+	return result, err
+}
+
+func addMoney(
+	ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64,
+) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID1,
+		Amount: amount1,
+	})
+	if err != nil {
+		return
+	}
+
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     accountID2,
+		Amount: amount2,
+	})
+	return
+}
